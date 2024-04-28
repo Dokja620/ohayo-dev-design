4aba016ed31e4e1e68196df045e48d9a3d4766f1
HomeJa <revanamike620@gmail.com>
2024-04-28 23:01:06 +0000

    🦄 refactor(navbar): working
    made it works the way I wanted it to

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -6,6 +6,21 @@ export default component$(() => {
     // TODO exit menu whenever I click outside when the check box is checked watch then uncheck when outside is clicked
     return (
         <header>
+            <div class="menu-btn">
+                <input type="checkbox" name="" id="nav-menu" />
+                <label class="toogle-menu" for="nav-menu"></label>
+                <div class="burger toggle">
+                    <div class="bur-line"></div>
+                    <div class="bur-line"></div>
+                    <div class="bur-line"></div>
+                </div>
+                <div class="burger-links">
+                    <Link href="/pricing">Nos prix</Link>
+                    <Link href="/réalisation">Nos réalisations</Link>
+                    <Link href="/publication">Nos publications</Link>
+                    <Link href="/more">À-propos</Link>
+                </div>
+            </div>
             <nav>
                 <div class="logo"><img src={logo} alt="Logo" width={50} height={50} /></div>
                 <div class="nav-links">
@@ -16,18 +31,9 @@ export default component$(() => {
                             <Link href="/commande" class="command">Commander</Link>
                         </div>
                         <div class="menu second">
-                            <input type="checkbox" name="" id="" />
-                            <div class="burger">
-                                <div class="bur-line"></div>
-                                <div class="bur-line"></div>
-                                <div class="bur-line"></div>
+                            <div class="burger" >
                             </div>
-                            <div class="burger-links">
-                                <Link href="/pricing">Nos prix</Link>
-                                <Link href="/réalisation">Nos réalisations</Link>
-                                <Link href="/publication">Nos publications</Link>
-                                <Link href="/more">À-propos</Link>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
