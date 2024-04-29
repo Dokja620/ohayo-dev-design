ddf5903c4ac71dbee505263415ebd3e4f71f4212
HomeJa <revanamike620@gmail.com>
2024-04-29 20:55:13 +0000

    🦄 refactor(navbar): updates
    changed whole thing… it's better now😎
    
    still need to make this responsive.

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,6 +1,10 @@
 import { component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
 import logo from '/media/logo.webp';
+import infoLogo from "../icons/IcOutlineInfo.svg"
+import serviceLogo from "../icons/IcSharpMiscellaneousServices.svg"
+import workLogo from "../icons/IcTwotoneWorkOutline.svg"
+import postLogo from "../icons/IcOutlinePhotoSizeSelectActual.svg"
 
 export default component$(() => {
     // TODO exit menu whenever I click outside when the check box is checked watch then uncheck when outside is clicked
@@ -15,10 +19,22 @@ export default component$(() => {
                     <div class="bur-line"></div>
                 </div>
                 <div class="burger-links">
-                    <Link href="/pricing">Nos prix</Link>
-                    <Link href="/réalisation">Nos réalisations</Link>
-                    <Link href="/publication">Nos publications</Link>
-                    <Link href="/more">À-propos</Link>
+                    <div class="link">
+                        <div class="ico-nav"><img src={serviceLogo} alt="info icon" width={20} height={20}/></div>
+                        <Link href="/pricing">Nos services</Link>
+                    </div>
+                    <div class="link">
+                        <div class="ico-nav"><img src={workLogo} alt="info icon" width={20} height={20}/></div>
+                        <Link href="/réalisation">Nos réalisations</Link>
+                    </div>
+                    <div class="link">
+                        <div class="ico-nav"><img src={postLogo} alt="info icon" width={20} height={20}/></div>
+                        <Link href="/publication">Nos publications</Link>
+                    </div>
+                    <div class="link">
+                        <div class="ico-nav"><img src={infoLogo} alt="info icon" width={20} height={20}/></div>
+                        <Link href="/more">À-propos</Link>
+                    </div>
                 </div>
             </div>
             <nav>
@@ -28,16 +44,12 @@ export default component$(() => {
                     
                     <div class="links">
                         <div class="menu">
-                            <Link href="/commande" class="command">Commander</Link>
+                            <Link href="/commande" class="command">Commencez maintenant</Link>
                         </div>
-                        <div class="menu second">
-                            <div class="burger" >
-                            </div>
-                            
+                        <div class="second">
                         </div>
                     </div>
                 </div>
-
             </nav>
         </header>
     );
