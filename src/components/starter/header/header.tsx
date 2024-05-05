9a3d6fe64d459330e69d822f7e63afcdfa3e5a5b
HomeJa <revanamike620@gmail.com>
2024-05-05 02:19:10 +0000

    🦄 refactor(everything from now on): re-design
    new layout, new navbar and new fonts and so on…

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,59 +1,37 @@
 import { component$ } from '@builder.io/qwik';
 import { Link } from '@builder.io/qwik-city';
-import logo from '/media/logo.webp';
-import infoLogo from "../icons/IcBaselineInfo.svg"
-import serviceLogo from "../icons/IcSharpMiscellaneousServices.svg"
-import workLogo from "../icons/IcRoundFolderShared.svg"
-import postLogo from "../icons/IcBaselinePhotoSizeSelectActual.svg"
-import orderLogo from "../icons/IcBaselineAppRegistration.svg"
+import logo from '/media/logo-md-1.webp';
+// import infoLogo from "../icons/IcBaselineInfo.svg"
+// import serviceLogo from "../icons/IcSharpMiscellaneousServices.svg"
+// import workLogo from "../icons/IcRoundFolderShared.svg"
+// import postLogo from "../icons/IcBaselinePhotoSizeSelectActual.svg"
+// import orderLogo from "../icons/IcBaselineAppRegistration.svg"
 
 export default component$(() => {
     // TODO exit menu whenever I click outside when the check box is checked watch then uncheck when outside is clicked
     return (
         <header>
-            <div class="menu-btn">
-                <input type="checkbox" name="" id="nav-menu" />
-                <label class="toogle-menu" for="nav-menu"></label>
-                <div class="burger toggle">
-                    <div class="bur-line"></div>
-                    <div class="bur-line"></div>
-                    <div class="bur-line"></div>
-                </div>
-                <div class="burger-links">
-                    <div class="link">
-                        <div class="ico-nav"><img src={orderLogo} alt="info icon" width={20} height={20}/></div>
-                        <Link href="/concevoir-site-web">Commander</Link>
-                    </div>
-                    <div class="link">
-                        <div class="ico-nav"><img src={serviceLogo} alt="info icon" width={20} height={20}/></div>
-                        <Link href="/services-web">Nos services</Link>
-                    </div>
-                    <div class="link">
-                        <div class="ico-nav"><img src={workLogo} alt="info icon" width={20} height={20}/></div>
-                        <Link href="/portefolio">Nos réalisations</Link>
-                    </div>
-                    <div class="link">
-                        <div class="ico-nav"><img src={postLogo} alt="info icon" width={20} height={20}/></div>
-                        <Link href="/publication-web">Nos publications</Link>
-                    </div>
-                    <div class="link">
-                        <div class="ico-nav"><img src={infoLogo} alt="info icon" width={20} height={20}/></div>
-                        <Link href="/a-propos">À-propos</Link>
-                    </div>
-                </div>
-            </div>
             <nav>
-                <div class="logo"><Link href="/"><img src={logo} alt="Logo" width={50} height={50} /></Link></div>
+                <div class="logo">
+                    <Link href='/' class="ico-planet"><img src={logo} alt="aze" width={378} height={172}/></Link>
+                </div>
+
                 <div class="nav-links">
-                    <Link href="/" class="go-home">Acceuil</Link>
-                    
-                    <div class="links">
-                        <div class="menu">
-                            <Link href="/concevoir-site-web" class="command">Commencez</Link>
-                        </div>
-                        <div class="second">
-                        </div>
-                    </div>
+                    <div class="link"><Link href='/'>Réalisations</Link></div>
+                    <div class="link"><Link href='/'>Services</Link></div>
+                    <div class="link"><Link href='/'>Publications</Link></div>
+                    <div class="link"><Link href='/'>A-propos</Link></div>
+                </div>
+
+                <div class="link command"><Link href='/'>Concevoir</Link></div>
+
+                <input type="checkbox" name="burger" id="burger-toggle" class="burger-btn"/>
+                <label for="burger-toggle" id="toggle-off"></label>
+
+                <div class="burger-menu">
+                    <span></span>
+                    <div><span></span><span></span></div>
+                    <span></span>
                 </div>
             </nav>
         </header>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,261 +1,67 @@
-import { component$, useStore, useVisibleTask$, $} from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { Link } from '@builder.io/qwik-city';
 import type { DocumentHead } from "@builder.io/qwik-city";
+
 import planet from "/media/home/planet.webp";
 import spatio from "/media/home/spatio.webp";
-import spatioStand from "/media/home/spatio-stand.webp";
-import arrow from "/media/home/IcOutlineArrowBackIosNew.svg";
-import arrowr from "/media/home/IcOutlineArrowForwardIosNew.svg";
 
-
-// tech stack of the agency
-import tech_a from "/media/tech/3d/001.webp";
-import tech_b from "/media/tech/3d/002.webp";
-import tech_c from "/media/tech/3d/003.webp";
-import tech_d from "/media/tech/3d/004.webp";
-import tech_e from "/media/tech/3d/005.webp";
-import tech_f from "/media/tech/3d/006.webp";
-import tech_g from "/media/tech/3d/007.webp";
-import tech_h from "/media/tech/3d/008.webp";
-import tech_i from "/media/tech/3d/009.webp";
-import tech_j from "/media/tech/3d/010.webp";
-import tech_k from "/media/tech/3d/011.webp";
-import tech_l from "/media/tech/3d/012.webp";
-import tech_m from "/media/tech/3d/013.webp";
-import tech_n from "/media/tech/3d/014-001.png";
-import tech_o from "/media/tech/3d/015.webp";
-import tech_p from "/media/tech/3d/016.webp";
-import tech_q from "/media/tech/3d/017.webp";
-import tech_r from "/media/tech/3d/018.webp";
-import tech_s from "/media/tech/3d/019.webp";
-import tech_t from "/media/tech/3d/020.webp";
-import tech_u from "/media/tech/3d/021.webp";
-// import tech_v from "/media/tech/3d/022.webp";
-import tech_w from "/media/tech/3d/023.webp";
-import tech_x from "/media/tech/3d/024.webp";
-import tech_y from "/media/tech/3d/025.webp";
-import tech_z from "/media/tech/3d/026.webp";
-import tech_A from "/media/tech/3d/027.webp";
-import tech_B from "/media/tech/3d/028.webp";
-import tech_C from "/media/tech/3d/029.webp";
-import tech_D from "/media/tech/3d/030.webp";
-import tech_E from "/media/tech/3d/031.webp";
+// Import images dynamically if possible, or define base path and use it in src
+const basePath = "/media/tech/";
 
 export default component$(() => {
+    // Array of tool objects
+    const tools = Array.from({ length: 37 }, (_, i) => ({
+        src: `${basePath}${String(i + 1).padStart(3, '0')}.svg`,
+        alt: `Tool ${i + 1}`
+    }));
 
-    const state = useStore({ currentSlide: 1, maxSlides: 11 });
-
-    // eslint-disable-next-line qwik/no-use-visible-task
-    useVisibleTask$(() => {
-        const updateSlide = () => {
-            state.currentSlide = (state.currentSlide % state.maxSlides) + 1;
-        };
-
-        const intervalId = setInterval(updateSlide, 4000);
-
-        return () => clearInterval(intervalId);
-    });
-
-    const changeStep = $((index: number) => {
-        state.currentSlide = index; // Directly update the state
-        const slideEl = document.getElementById('slider');
-        if (slideEl) {
-            slideEl.setAttribute('slide-step', index.toString());
-        } else {
-            console.error('Slider element not found');
-        }
-    });
-
-  
     return (
         <>
-            {/* first section intro to who we are */}
-            <section id="home-1" class="container">
+            {/* Hero section */}
+            <section id="hero" class="container">
                 <div class="content">
-                    <h1>Voulez-vous vous construire une présence en ligne ?</h1>
-                    <h2>Vous êtes au bon endroit : Bienvenue sur Ohayo Dev & Design !</h2>
+                    <div class="intro">
+                        <h1>Rendez votre entreprise visible à l'international</h1>
+                        <p>Démarrez dès maintenant la conception de votre site web avec Ohayo Dev & Design pour viser un public mondial avec un service accessible partout 24h/24 et 07j/07.</p>
+                    </div>
+                    <div class="c-t-a">
+                        <span><Link href='/'>Démarrez maintenant</Link></span>
+                        <span><Link href='/'>Contact</Link></span>
+                    </div>
+                </div>
 
-                    <div class="text">
-                        <p>
-                            Notre agence spécialisée en développement web reunit une équipe
-                            de freelances aux compétence diversifié, essentielles à la
-                            conception de votre site internet. Nous offrons un éventail de
-                            services allant du développement  Front-End et Back-End au design UX/UI,
-                            sans oublier la création de contenu et l'optimisation pour les moteurs de
-                            recherche (SEO). Cette approche intégré nous permet de matérialiser avec
-                            précision vos projets, en convertissant vos idées en solutions numériques
-                            innovantes. Que votre objectif soit de lancer un site vitrine élégant ou 
-                            une plateforme complexe dotée de multiples fonctionnalités, nous sommes là
-                            pour transformer votre vision en succés. Confiez-nous votre projet et propulsez
-                            votre marque sur la scène internationale avec une présence en ligne ininterrompue
-                            disponible 24 heures sur 24, 7 jours sur 7.
-                        </p>
-                    </div>
+                <div class="bg-h planet"><img src={planet} alt="" width={894} height={894}/></div>
+                <div class="bg-h spatio"><img src={spatio} alt="" width={621} height={826}/></div>
+            </section>
 
-                    <div class="deco-home spatios">
-                        <img src={spatio} alt="" height={826} width={621}/>
-                    </div>
-                    <div class="deco-home planets">
-                        <img src={planet} alt="" height={894} width={894}/>
-                    </div>
-                </div>
-            </section>
-            
-            {/* second section */}
-            <section id="home-2" class="container">
+            {/* Infinite skills slider section */}
+            <div id="scroll">
+                <h2>Nous maitrisons</h2>
                 <div class="content">
-                    <div class="tech">
-                        <h2>Nous maitrisons:</h2>
-                        <div class="tools">
-                            <div><img src={tech_a} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_b} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_c} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_d} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_e} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_f} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_g} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_h} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_i} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_j} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_k} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_l} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_m} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_n} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_o} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_p} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_q} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_r} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_s} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_t} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_u} alt="tech we use" width={50} height={50}/></div>
-                            {/* <div><img src={tech_v} alt="tech we use" width={50} height={50}/></div> */}
-                            <div><img src={tech_w} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_x} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_y} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_z} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_A} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_B} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_C} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_D} alt="tech we use" width={50} height={50}/></div>
-                            <div><img src={tech_E} alt="tech we use" width={50} height={50}/></div>
-                        </div>
-                    </div>
-                    <div class="fact">
-                        <div class="fact-box">
-                            <h2>Pourquoi nous choisir ?</h2>
-                            <div class="slider" id="slider" slide-step={state.currentSlide}>
-                                <div class="wrap slides-1">
-                                    <h3>Responsive Design :</h3>
-                                    <p>Tous les sites web que nous concevons sont garantis 100% responsives, assurant une expérience utilisateur optimale sur tous les appareils.</p>
-                                </div>
-                                <div class="wrap slides-2">
-                                    <h3>Site web sécurisé :</h3>
-                                    <p>Chaque site que nous développons intègre des mesures de sécurité, y compris un certificat SSL.</p>
-                                </div>
-                                <div class="wrap slides-3">
-                                    <h3>Site web optimisisé :</h3>
-                                    <p>Nous prenons en charge l'optimisation de chaque site web que nous créons pour améliorer ses performances et sa visibilité.</p>
-                                </div>
-                                <div class="wrap slides-4">
-                                    <h3>Expertise spécialisée :</h3>
-                                    <p>Notre équipe comprend des spécialistes dans tous les domaines nécessaires pour optimiser votre site web au maximum et répondre à tous vos besoins.</p>
-                                </div>
-                                <div class="wrap slides-5">
-                                    <h3>Flexibilité de développement :</h3>
-                                    <p>Selon vos besoins, moyens et préférences, vous avez le choix entre un développement full code pour une personnalisation maximale ou low code pour une mise en œuvre rapide.</p>
-                                </div>
-                                <div class="wrap slides-6">
-                                    <h3>Ressources supplémentaires :</h3>
-                                    <p>Nous mettons  à la disposition de bos clients un e-book gratuit pour répondre à toutes vos questions, vous guidant à travers chaque étape de la conception de votre site.</p>
-                                </div>
-                                <div class="wrap slides-7">
-                                    <h3>Analyse personnalisée :</h3>
-                                    <p>Nous pouvons offrir une analyse détaillée de votre site existant, si nécessaire, pour identifier les améliorations potentielles.</p>
-                                </div>
-                                <div class="wrap slides-8">
-                                    <h3>Facilités de paiement :</h3>
-                                    <p>Nous offrons la possibilité de régler en plusieurs fois pour faciliter l'accès à nos services.</p>
-                                </div>
-                                <div class="wrap slides-9">
-                                    <h3>Écoute et personnalisation :</h3>
-                                    <p>Notre équipe travaille en étroite collaboration avec vous pour s'assurer que le site final reflète parfaitement votre vision, prête à effectuer les ajustements nécessaires jusqu'à votre entière satisfaction.</p>
-                                </div>
-                                <div class="wrap slides-10">
-                                    <h3>Support continu :</h3>
-                                    <p>La possibilité d'engager nos spécialistes à tout moment du projet assure un soutien constant et une adaptabilité à vos besoins évolutifs.</p>
-                                </div>
-                                <div class="wrap slides-11">
-                                    <h3>Site web sécurisé :</h3>
-                                    <p>Pour ceux qui en ont besoin, nous prenons en charge le processus d'hébergement de votre site, gratuitement.</p>
-                                </div>
-                                <div class="pagination">
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(1);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(2);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(3);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(4);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(5);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(6);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(7);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(8);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(9);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(10);}}></div>
-                                    <div onClick$={(event) => {event.preventDefault(); changeStep(11);}}></div>
-                                </div>
-                                <div class="navigation">
-                                    <div class="left" onClick$={(event) => {
-                                        event.preventDefault();
-                                        changeStep(state.currentSlide > 1 ? state.currentSlide - 1 : state.maxSlides);
-                                    }}>
-                                        <img src={arrow} alt="back arrow" width={50} height={50}/>
-                                    </div>
-                                    <div class="right" onClick$={(event) => {
-                                        event.preventDefault();
-                                        changeStep(state.currentSlide < state.maxSlides ? state.currentSlide + 1 : 1);
-                                    }}>
-                                        <img src={arrowr} alt="forward arrow" width={50} height={50}/>
-                                    </div>
-                                </div>
+                    <div class="tools">
+                        {tools.map(tool => (
+                            <div key={tool.src}>
+                                <img src={tool.src} alt={tool.alt} width={85} height={85}/>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
-            </section>
-            
-            {/* last section */}
-            <section id="home-3" class="container">
+            </div>
+
+            <section id="hero" class="container">
                 <div class="content">
-                    <div class="portfolio">
-                        <h2>Voir nos portfolio :</h2>
-                        <div class="portfolios">
-                            <span class="front">
-                                <Link href="/">Développeur Front-End</Link>
-                            </span>
-                            <span>
-                                <Link href="/">Développeur Back-End</Link>
-                            </span>
-                            <span>
-                                <Link href="/">UX / UI Designer</Link>
-                            </span>
-                            <span>
-                                <Link href="/">Rédacteur Web</Link>
-                            </span>
-                            <span>
-                                <Link href="/">SEO Analyst</Link>
-                            </span>
-                        </div>
+                    <div class="intro">
+                        <h1>Rendez votre entreprise visible à l'international</h1>
+                        <p>Démarrez dès maintenant la conception de votre site web avec Ohayo Dev & Design pour viser un public mondial avec un service accessible partout 24h/24 et 07j/07.</p>
                     </div>
-                    <div class="navigation">
-                        <div class="home-links">
-                            <Link href="/">Faire une commande</Link>
-                            <Link href="/">Aperçu de notre travail</Link>
-                            <Link href="/">Nos publications</Link>
-                            <Link href="/">Qui sommes-nous ?</Link>
-                            <div class="spatio-home">
-                                <img src={spatioStand} alt="standing spationaute" width={323} height={934}/>
-                            </div>
-                        </div>
+                    <div class="c-t-a">
+                        <span><Link href='/'>Démarrez maintenant</Link></span>
+                        <span><Link href='/'>Contact</Link></span>
                     </div>
                 </div>
+
+                <div class="bg-h planet"><img src={planet} alt="" width={894} height={894}/></div>
+                <div class="bg-h spatio"><img src={spatio} alt="" width={621} height={826}/></div>
             </section>
         </>
     );
