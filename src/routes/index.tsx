a75cadbcbb4e4d6f82f701c7f202e06b43eb3b64
HomeJa <revanamike620@gmail.com>
2024-05-06 08:29:04 +0300

    🦄 refactor(_home): new layout
    change the layout but apart from that it's basically the same as before.
    
    let's deal with responsiveness

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,14 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 import planet from "/media/home/planet.webp";
 import spatio from "/media/home/spatio.webp";
+import spatioStand from "/media/home/spatio-stand.webp";
 
 // Import images dynamically if possible, or define base path and use it in src
 const basePath = "/media/tech/";
 
 export default component$(() => {
     // Array of tool objects
-    const tools = Array.from({ length: 37 }, (_, i) => ({
+    const tools = Array.from({ length: 36 }, (_, i) => ({
         src: `${basePath}${String(i + 1).padStart(3, '0')}.svg`,
         alt: `Tool ${i + 1}`
     }));
@@ -18,7 +19,7 @@ export default component$(() => {
     return (
         <>
             {/* Hero section */}
-            <section id="hero" class="container">
+            <section id="hero">
                 <div class="content">
                     <div class="intro">
                         <h1>Rendez votre entreprise visible à l'international</h1>
@@ -26,7 +27,7 @@ export default component$(() => {
                     </div>
                     <div class="c-t-a">
                         <span><Link href='/'>Démarrez maintenant</Link></span>
-                        <span><Link href='/'>Contact</Link></span>
+                        {/* <span><Link href='/'>Contact</Link></span> */}
                     </div>
                 </div>
 
@@ -34,34 +35,64 @@ export default component$(() => {
                 <div class="bg-h spatio"><img src={spatio} alt="" width={621} height={826}/></div>
             </section>
 
+            {/* Why us section */}
+            <section id="reason" class="container">
+                <div class="portfolio">
+                    <h2>Voir nos portfolio :</h2>
+                    <div class="portfolios">
+                        <span class="front">
+                            <Link href="/">Développeur Front-End</Link>
+                        </span>
+                        <span>
+                            <Link href="/">Développeur Back-End</Link>
+                        </span>
+                        <span>
+                            <Link href="/">UX / UI Designer</Link>
+                        </span>
+                        <span>
+                            <Link href="/">Rédacteur Web</Link>
+                        </span>
+                        <span>
+                            <Link href="/">SEO Analyst</Link>
+                        </span>
+                    </div>
+                    <div class="spatio-stand">
+                        <img src={spatioStand} alt="standing spationaute" width={323} height={934}/>
+                    </div>
+                </div>
+                <div class="content fact">
+                    <h2>Pourquoi nous ?</h2>
+                    <div class="slider" id="slider">
+                        <div class="wrap slides-9">
+                            <h3>Écoute et personnalisation :</h3>
+                            <p>Notre équipe travaille en étroite collaboration avec vous pour s'assurer que le site final reflète parfaitement votre vision, prête à effectuer les ajustements nécessaires jusqu'à votre entière satisfaction.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
             {/* Infinite skills slider section */}
-            <div id="scroll">
-                <h2>Nous maitrisons</h2>
+            <section id="scroll">
                 <div class="content">
+                    {/* <h2>Nous maitrisons :</h2> */}
                     <div class="tools">
                         {tools.map(tool => (
                             <div key={tool.src}>
-                                <img src={tool.src} alt={tool.alt} width={85} height={85}/>
+                                <img src={tool.src} alt={tool.alt} width={75} height={75}/>
                             </div>
                         ))}
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <section id="hero" class="container">
-                <div class="content">
-                    <div class="intro">
-                        <h1>Rendez votre entreprise visible à l'international</h1>
-                        <p>Démarrez dès maintenant la conception de votre site web avec Ohayo Dev & Design pour viser un public mondial avec un service accessible partout 24h/24 et 07j/07.</p>
-                    </div>
-                    <div class="c-t-a">
-                        <span><Link href='/'>Démarrez maintenant</Link></span>
-                        <span><Link href='/'>Contact</Link></span>
-                    </div>
+            {/* Navigation */}
+            <section id="visit" class="container">
+                <div class="home-links">
+                    <Link href="/">Qui sommes-nous ?</Link>
+                    <Link href="/">Aperçu de notre travail</Link>
+                    <Link href="/">Nos publications</Link>
+                    <Link href="/">Faire une commande</Link>
                 </div>
-
-                <div class="bg-h planet"><img src={planet} alt="" width={894} height={894}/></div>
-                <div class="bg-h spatio"><img src={spatio} alt="" width={621} height={826}/></div>
             </section>
         </>
     );
