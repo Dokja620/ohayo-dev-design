2292f89583f13c7645b81c99e08264baefdadc7b
HomeJa <revanamike620@gmail.com>
2024-05-24 21:43:42 +0300

    🌈 style(base style): 🙃 form
    it looks ok, but just ok
    
    big issue 👀 it clears each steps after it goes in other step

--- src/routes/concevoir-site-web/steps/step02.tsx
+++ src/routes/concevoir-site-web/steps/step02.tsx
@@ -3,7 +3,43 @@ import { component$, Slot } from "@builder.io/qwik";
 export const Step02 = component$(() => {
     return (
         <div class="step-2">
-            <h2>test echo 02</h2>
+            <h2>De quel genre de site avez vous besoin ?</h2>
+            <div class="second-step">
+                <p>Type de site :</p>
+                <div class="site-type">
+                    <span>
+                        <input type="radio" name="site-type" id="full-code" />
+                        <label for="full-code">Site Full-code</label>
+                    </span>
+                    <span>
+                        <input type="radio" name="site-type" id="low-code" />
+                        <label for="low-code">Site Low-code</label>
+                    </span>
+                </div>
+                <p>Services supplémentaires :</p>
+                <div class="options">
+                    <span>
+                        <input type="checkbox" name="responsive" id="ui-design" />
+                        <label for="ui-design">Avec UI Designer</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" name="responsive" id="redactor" />
+                        <label for="redactor">Avec Rédacteur</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" name="responsive" id="back-end" />
+                        <label for="back-end">Avec Back-End</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" name="responsive" id="seo" />
+                        <label for="seo">Avec SEO</label>
+                    </span>
+                    <span>
+                        <input type="checkbox" name="responsive" id="extra-services" />
+                        <label for="extra-services">Sans service supplémentaires</label>
+                    </span>
+                </div>
+            </div>
             <Slot />
         </div>
     )
--- src/routes/concevoir-site-web/steps/step04.tsx
+++ src/routes/concevoir-site-web/steps/step04.tsx
@@ -3,7 +3,10 @@ import { component$, Slot } from "@builder.io/qwik";
 export const Step04 = component$(() => {
     return (
         <div class="step-4">
-            <h2>test echo 04</h2>
+            <p>Parlez nous du site que vous voulez créer avec le plus de détails possible.</p>
+            <div class="fourth-step">
+                <textarea name="message" id="text-area" placeholder="Votre message…" rows={10}></textarea>
+            </div>
             <Slot />
         </div>
     )
